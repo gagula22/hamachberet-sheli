72b4ce5e4249a0eb9cadc8d1c470e4573812aa81
Moshe <gagula22@gmail.com>
2026-04-25 19:37:10 +0300

    Initial commit - המחברת שלי

---
+++ js/firebase-config.js
@@ -0,0 +1,8 @@
+window.FIREBASE_CONFIG = {
+  apiKey:            "AIzaSyCIYvUtp9eoRAIeyOe9gQHPdgSNfxl8oHs",
+  authDomain:        "my-notebook-b5229.firebaseapp.com",
+  projectId:         "my-notebook-b5229",
+  storageBucket:     "my-notebook-b5229.firebasestorage.app",
+  messagingSenderId: "409339902450",
+  appId:             "1:409339902450:web:bbdfb6ab2960653403536b"
+};
